349f286349b443c6e6775710cc8d68b22cec920c
umitdrmz22-svg <umitdrmz22@gmail.com>
2026-08-05 15:29:47 +0200

    Lokale Standardkonfiguration hinzufügen

---
+++ config.js
@@ -0,0 +1,10 @@
+window.UNFALL_CONFIG = {
+  appName: "Unfallmanagement Studio",
+  mode: "local",
+  supabaseUrl: "",
+  supabaseAnonKey: "",
+  functionsBaseUrl: "",
+  siteName: "Musterbetrieb",
+  defaultReminderWeekday: 1,
+  legalReviewDate: "2026-08-05"
+};
